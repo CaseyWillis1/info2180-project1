84921f9f492a38132b8ebdf01fac8211a6fd2c45
CaseyWillis1 <caseywillis140@gmail.com>
2021-10-24 17:48:31 -0500

    Edited scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-/* Add your JavaScript to this file */
+window.onload = function() {
+    var subForm = document.querySelector("button");
+    var msg = document.getElementsByClassName("message");
+    let email = document.getElementById("#email");
+    console.log(msg);
+
+    subForm.onclick = function() {
+        //do stuff
+        var message = () => `Thank you! Your email address ${email} has been added to our mailing list! `;
+        msg.innerHTML = message();
+
+        //window.alert("This color is blue!");
+        console.log(msg);
+    }
+
+    //myElem.onclick=colorFunction;
+};
